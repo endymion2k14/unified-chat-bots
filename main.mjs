f97dccd2508bd1ad2aa6f9108357dbc6d5b6e7fe
YOomDev <ype.oomes@gmail.com>
2025-09-14 20:32:53 +0200

    Change main into mjs for module support and implement most of main

---
+++ main.mjs
@@ -0,0 +1,32 @@
+import { log, json } from './utils.mjs';
+
+import { ClientTwitch } from "./bots/twitch/bot.mjs";
+
+const SOURCE = "MAIN";
+
+async function start() {
+    log.info("Loading settings");
+    const settings = json.load("configs/secrets.json");
+
+    // Check which bots to run
+    const botsTwitch  = [];
+    const botsDiscord = [];
+    for (let i = 0; i < settings.discord.length; i++) { if (settings.discord[i].enabled) { botsDiscord.push(i); } }
+    for (let i = 0; i < settings.twitch .length; i++) { if (settings.twitch [i].enabled) { botsTwitch .push(i); } }
+
+    // Run the bots
+    const clientsDiscord = [];
+    const clientsTwitch = [];
+    if (botsDiscord.length > 0) {
+        log.info(`Starting ${botsDiscord.length} discord bots`, SOURCE);
+        for (let i = 0; i < botsDiscord.length; i++) {
+            // TODO
+        }
+    }
+    if (botsTwitch .length > 0) {
+        log.info(`Starting ${botsTwitch .length} twitch bots`, SOURCE);
+        for (let i = 0; i < botsTwitch.length; i++) { clientsTwitch.push(new ClientTwitch(settings.twitch[botsTwitch[i]])); }
+    }
+}
+
+start().catch(err => { log.error(err); });
