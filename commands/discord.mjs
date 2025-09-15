3a4302c11370ab61138af274e200f40363254a38
YOomDev <ype.oomes@gmail.com>
2025-09-15 15:11:41 +0200

    Implement command

---
+++ commands/discord.mjs
@@ -0,0 +1,8 @@
+import { json } from "../utils.mjs";
+
+const config = json.load('../configs/discord.json'); // TODO: make it possible for different discord links based on which bot is prompted
+
+export default {
+    name: 'discord',
+    async reply(params, client, event) { client.sendMessage(config.text.toString()); },
+};
