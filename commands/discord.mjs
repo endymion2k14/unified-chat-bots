ade9ee8f6dbf7326f50affd94db0b8513f819ad8
YOomDev <ype.oomes@gmail.com>
2025-09-15 15:18:29 +0200

    Fix loading from wrong folder

--- commands/discord.mjs
+++ commands/discord.mjs
@@ -1,6 +1,6 @@
 import { json } from "../utils.mjs";
 
-const config = json.load('../configs/discord.json'); // TODO: make it possible for different discord links based on which bot is prompted
+const config = json.load('configs/discord.json'); // TODO: make it possible for different discord links based on which bot is prompted
 
 export default {
     name: 'discord',
