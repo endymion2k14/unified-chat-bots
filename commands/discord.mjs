c6ae868d252cafcc3d6dc68c0ac90b15228a51de
YOomDev <ype.oomes@gmail.com>
2025-09-22 17:18:40 +0200

    Remove old unused imports

--- commands/discord.mjs
+++ commands/discord.mjs
@@ -1,7 +1,3 @@
-import { json } from '../utils.mjs';
-
-const config = json.load('configs/discord.json'); // TODO: make it possible for different discord links based on which bot is prompted
-
 export default {
     name: 'discord',
     async reply(params, client, event) { client.sendMessage(client.getCommandConfig(name)); },
