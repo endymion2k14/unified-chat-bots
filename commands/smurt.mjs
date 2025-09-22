723b647daccf71371ce49958195fed5e9d2626f3
YOomDev <ype.oomes@gmail.com>
2025-09-22 18:18:18 +0200

    Update command to make use of the command config file

--- commands/smurt.mjs
+++ commands/smurt.mjs
@@ -2,13 +2,9 @@ import { randomInt } from '../utils.mjs';
 
 export default {
     name: 'smurt',
-    async reply(params, client, event) { client.sendMessage(messages[randomInt(0,  messages.length) % messages.length]); } // TODO: test if it is inclusive and remove modulo if possible
+    async reply(params, client, event) {
+        const messages = client.getCommandConfig(name);
+        if (messages.length > 0) { client.sendMessage(messages[randomInt(0,  messages.length)]); }
+        else { client.sendMessage("Smurt!"); }
+    }
 };
-
-const messages = [
-    'Smurt!',
-    'Very smurt indeed!',
-    'That\'s very smurt!',
-    'You are very smurt!',
-    'Smurt move!'
-];
