5a951a7579afa797cbd03c9909b62a0976b59fb9
YOomDev <ype.oomes@gmail.com>
2025-09-22 18:43:50 +0200

    Implement custom messages through the new config system

--- systems/welcome.mjs
+++ systems/welcome.mjs
@@ -1,26 +1,25 @@
 import { EventTypes } from "../bots/twitch/irc.mjs";
-import { equals } from "../utils.mjs";
+import { equals, randomInt } from "../utils.mjs";
 
 export default {
     name: 'welcome',
     init(client) {
         client.addListener(EventTypes.message, event => this.welcome(client, event));
         // TODO: implement stream shutdown/start resetting the chatted array
-        // TODO: implement custom messages
+        this.config = client.getSystemConfig(name);
     },
     chatted: [],
     welcome(client, event) {
         const user = event.username.toLowerCase();
         if (event.tags['first-msg']) {
-            client.sendMessage(`Welcome to the channel ${event.username}!`);
+            client.sendMessage(this.config.first[randomInt(0, this.config.first.length)].replaceAll('{USER}', event.username));
             this.chatted.push(user);
         }
         else {
-
             for (let i = 0; i < this.chatted.length; i++) {
                 if (equals(this.chatted[i], user)) { return; } // Return if user has already chatted
             }
-            client.sendMessage(`Welcome back ${event.username}!`);
+            client.sendMessage(this.config.back[randomInt(0, this.config.back.length)].replaceAll('{USER}', event.username));
             this.chatted.push(user);
         }
     }
