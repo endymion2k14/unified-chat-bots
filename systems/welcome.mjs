8aac02065b25ec3a298d45606c6660e0bf7b1d1f
YOomDev <ype.oomes@gmail.com>
2025-09-23 19:46:36 +0200

    Fix welcome system always first time welcome message no matter if the person has been in the channel before or not

--- systems/welcome.mjs
+++ systems/welcome.mjs
@@ -10,16 +10,15 @@ export default {
     },
     chatted: [],
     welcome(client, event) {
-        if (event.tags['first-msg']) { this.reply(client, this.config.first[randomInt(0, this.config.first.length)], event.username); }
+        if (event.tags['first-msg'] !== '0') { this.reply(client, this.config.first[randomInt(0, this.config.first.length)], event.username); }
         else {
-            const user = event.username.toLowerCase();
             for (let i = 0; i < this.chatted.length; i++) {
-                if (equals(this.chatted[i], user)) { return; } // Return if user has already chatted
+                if (equals(this.chatted[i], event.username)) { return; } // Return if user has already chatted
             }
             this.reply(client, this.config.back[randomInt(0, this.config.back.length)], event.username);
         }
     },
-    reply: function(client, message, user) {
+    reply(client, message, user) {
         client.sendMessage(message.replaceAll('{USER}', user));
         this.chatted.push(user);
     }
