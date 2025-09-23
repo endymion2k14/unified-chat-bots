df9517b638c762d31ddb2e83cd4d59b79b361e08
YOomDev <ype.oomes@gmail.com>
2025-09-23 18:04:22 +0200

    Remove unnecessary log

--- systems/botUptime.mjs
+++ systems/botUptime.mjs
@@ -3,10 +3,7 @@ import { getTimeDifference, log } from "../utils.mjs";
 export default {
     name: 'botUptime',
     startTime: 0,
-    init(client) {
-        log.info('upTime system is being initialized...', 'upTime');
-        this.start = this._now();
-    },
+    init(client) { this.start = this._now(); },
     _now() { return new Date().getTime(); },
     getUptimeValue() { return this.start - this._now(); },
     getUptimeText(shortened_words = true) { return getTimeDifference(this.getUptimeValue(), 0, shortened_words); }
