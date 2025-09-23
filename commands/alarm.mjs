e8a06e8de099d93dcd1930596264757c1998a34a
YOomDev <ype.oomes@gmail.com>
2025-09-23 17:55:10 +0200

    Make alarm use getTimeDifference instead of local custom implementation and remove some unneeded escape characters

--- commands/alarm.mjs
+++ commands/alarm.mjs
@@ -1,4 +1,4 @@
-import { sleep } from '../utils.mjs';
+import { getTimeDifference, sleep } from '../utils.mjs';
 
 export default {
     name: 'alarm',
@@ -10,16 +10,9 @@ export default {
         }
         if (params.length < 2) { client.sendMessage('Not enough arguments to run this command!'); return; }
         const name = params[0];
-        const number = parseInt(params[1]);
-        if (isNaN(number)) { client.sendMessage('Second argument is not a number!'); return; }
-        const total = Math.max(number, 0.017); // Clamp the timer to anything above one second
-        const hours = Math.floor(total / 60);
-        const minutes = Math.floor(total - (hours * 60));
-        const seconds = Math.floor((total - (hours * 60 + minutes)) * 60);
-        let time = hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''}`.toString() : '';
-        if (minutes > 0) { time += `${time.length > 0 ? ' and ' : ''}${minutes} minute${minutes > 1 ? 's' : ''}`.toString(); }
-        if (seconds > 0) { time += `${time.length > 0 ? ' and ' : ''}${seconds} second${seconds > 1 ? 's' : ''}`.toString(); }
-        client.sendMessage(`Timer \'${name}\' started for ${time}.`);
-        sleep(60 * total).then(_ => { client.sendMessage(`Timer \'${name}\' ended.`); });
+        const minutes = parseInt(params[1]);
+        if (isNaN(minutes)) { client.sendMessage('Second argument is not a number!'); return; }
+        client.sendMessage(`Timer '${name}' started for ${getTimeDifference(minutes * 60 * 1000)}.`);
+        sleep(60 * minutes).then(_ => { client.sendMessage(`Timer '${name}' ended.`); });
     }
 }
