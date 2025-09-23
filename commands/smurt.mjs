5d72b9db354fd725c8b4ebc3a44332f631b95b84
YOomDev <ype.oomes@gmail.com>
2025-09-23 20:26:24 +0200

    Fix missing this.

--- commands/smurt.mjs
+++ commands/smurt.mjs
@@ -3,7 +3,7 @@ import { randomInt } from '../utils.mjs';
 export default {
     name: 'smurt',
     async reply(params, client, event) {
-        const messages = client.getCommandConfig(name);
+        const messages = client.getCommandConfig(this.name);
         if (messages.length > 0) { client.sendMessage(messages[randomInt(0,  messages.length)]); }
         else { client.sendMessage("Smurt!"); }
     }
