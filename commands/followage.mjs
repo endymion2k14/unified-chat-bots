b18d5a244ef35475f7a9adf1040fa976b9450aba
YOomDev <ype.oomes@gmail.com>
2025-10-01 00:29:35 +0200

    Add command reply for requesting follow-age of the streamer, or for when the streamer requests their own follow-age

--- commands/followage.mjs
+++ commands/followage.mjs
@@ -1,4 +1,4 @@
-import { getTimeDifference } from "../utils.mjs";
+import { equals, getTimeDifference } from "../utils.mjs";
 
 export default {
     name: 'followage',
@@ -7,11 +7,13 @@ export default {
         const system = client.getSystem('followers');
         if ('getFollowerData' in system) {
             if (params.length < 1) {
+                if (event.privileges.broadcaster) { client.sendMessage(`Unable to obtain your follow-age, streamers are unable to follow themselves`); return; }
                 const followerData = system.getFollowerData(event.username);
                 if (followerData === 0) { client.sendMessage(`You can only get info from this command if you have been following for a certain time ${event.username}.`); }
                 else { client.sendMessage(`You (${event.username}) have been a follower for ${getTimeDifference(new Date().getTime(), followerData.time, false, false)}.`); }
             } else {
                 if (!event.privileges.moderator && !event.privileges.broadcaster && !event.privileges.super) { client.sendMessage('You need to be a moderator or higher to use this command!'); return; }
+                if (equals(client.channel.toLowerCase(), params[0].toLowerCase())) { client.sendMessage(`Unable to obtain the streamers follow-age, as streamers are unable to follow themselves`); return; }
                 const followerData = system.getFollowerData(params[0]);
                 if (followerData === 0) { client.sendMessage(`${params[0]} could not be found in the currently loaded list of followers...`); }
                 else { client.sendMessage(`${followerData.name} has been a follower for ${getTimeDifference(new Date().getTime(), followerData.time, false, false)}.`); }
