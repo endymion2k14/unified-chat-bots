0bf98e317f6b6c5aab2b5c348b5caac3abf77234
YOomDev <ype.oomes@gmail.com>
2025-10-01 21:07:48 +0200

    Added config option to change the default gemma3:4b-it-qat model for gpt responses

--- systems/gpt.mjs
+++ systems/gpt.mjs
@@ -3,15 +3,17 @@ import { Ollama } from 'ollama';
 export default {
     name: 'gpt',
     ollama: 0,
+    model: 'gemma3:4b-it-qat',
 
     init(client) {
         const config = client.getSystemConfig(this.name);
-        if ('host' in config) { this.ollama = new Ollama({ host: config.host }); }
+        if ('model' in config) { this.model  = config.model; }
+        if ('host'  in config) { this.ollama = new Ollama({ host: config.host }); }
         else { this.ollama = new Ollama(); }
     },
 
     async getResponse(prompt = '') {
         if (!this.ollama) { throw('Unable to contact GPT without its info being initialized.'); }
-        return await this.ollama.chat({ model: '', messages: [{ role: 'user', content: prompt }], stream: false });
+        return await this.ollama.chat({ model: this.model, messages: [{ role: 'user', content: prompt }], stream: false });
     }
 }
