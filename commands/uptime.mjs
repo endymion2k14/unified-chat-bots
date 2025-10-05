c8b18e9e4cc7bbdede23d281e2373881130d0f27
YOomDev <ype.oomes@gmail.com>
2025-10-05 20:55:34 +0200

    Make sure stream uptime is only shown when stream is live

--- commands/uptime.mjs
+++ commands/uptime.mjs
@@ -7,14 +7,15 @@ export default {
         if (params > 0) {
             if (equals(params[0].toLowerCase(), 'bot')) {
                 const uptime = client.getSystem('botUptime');
-                if ('getUptimeText' in uptime) { client.sendMessage(`Bot has been up for ${uptime.getUptimeText(false)}`); }
+                if ('getUptimeText' in uptime) { client.sendMessage(`Bot has been up for ${uptime.getUptimeText(false)}.`); }
                 else { throw('Problem getting info from the required system.'); }
                 return;
             }
         }
         const uptime = client.getSystem('channelLive');
         if ('getUptimeText' in uptime) {
-            client.sendMessage(`Bot has been up for ${uptime.getUptimeText(false)}`);
+            if (uptime._live) { client.sendMessage(`Stream has running for ${uptime.getUptimeText(false)}.`); }
+            else { client.sendMessage('Stream is currently offline.'); }
         }
         else { throw('Problem getting info from the required system.'); }
 
