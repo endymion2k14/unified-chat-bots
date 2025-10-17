a6000e8e012a3b0f33797f5d0059d9f3fe68be4f
endymion2k14 <roland@desktop-amd>
2025-10-17 23:20:53 +0200

    revert

--- commands/vanish.mjs
+++ commands/vanish.mjs
@@ -7,8 +7,7 @@ export default {
             event.privileges.subscriber  ||
             event.privileges.vip) {
                 if (event.privileges.broadcaster || event.privileges.moderator) { client.sendMessage(`Streamers and Moderators are unable to vanish themselves.`); return; }
-                const messages = await client.api.fetchMessagesFromUser(client, event);
-                for (const message of messages) { client.api.removeMessage(message.id); }
+                client.api.removeMessage(event.tags.id);
         } else { client.sendMessage(`You need to be at least a subscriber or VIP to use this command ${event.username}.`); }
     },
 };
