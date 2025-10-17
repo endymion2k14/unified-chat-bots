19cfe18173c9b027b22205dae67a04e55e35c24c
endymion2k14 <roland@desktop-amd>
2025-10-17 22:58:03 +0200

    this features:
    this implements:
    api: fetchMessagesFromUser
    api: clearchat, removeMessage,fetchMessagesFromUser (now uses Promise technology)
    command: !vanish (now removes all user chatter)

--- bots/twitch/api.mjs
+++ bots/twitch/api.mjs
@@ -35,12 +35,15 @@ export class TwitchAPI extends EventEmitter {
                 'Authorization': `Bearer ${this._data.token}`
             }
         }
-        let response = "";
-        https.get(options, r => {
-            r.setEncoding('utf8');
-            r.on('data', data => { response += data; });
-            r.on('end', _ => { });
-        }).on('error', err => { log.error(err); });
+        return new Promise((resolve, reject) => {
+            const req = https.request(options, res => {
+                let data = '';
+                res.setEncoding('utf8');
+                res.on('data', chunk => { data += chunk; });
+                res.on('end', () => { try { resolve(JSON.parse(data)); } catch (err) { reject(err); } });
+            }).on('error', err => { log.error(err); reject(err); });
+            req.end();
+        });
     }
     // Token is expected to have: moderator:manage:chat_messages
     async removeMessage(messageId) {
@@ -53,12 +56,35 @@ export class TwitchAPI extends EventEmitter {
                 'Authorization': `Bearer ${this._data.token}`
             }
         }
-        let response = "";
-        https.get(options, r => {
-            r.setEncoding('utf8');
-            r.on('data', data => { response += data; });
-            r.on('end', _ => { });
-        }).on('error', err => { logError(err); });
+        return new Promise((resolve, reject) => {
+            const req = https.request(options, res => {
+                let data = '';
+                res.setEncoding('utf8');
+                res.on('data', chunk => { data += chunk; });
+                res.on('end', () => { try { resolve(JSON.parse(data)); } catch (err) { reject(err); } });
+            }).on('error', err => { log.error(err); reject(err); });
+            req.end();
+        });
+    }
+
+    async fetchMessagesFromUser(client, event) {
+        const options = {
+            hostname: 'api.twitch.tv',
+            method: 'GET',
+            path: `/helix/chat/history?broadcaster_id=${client._data.roomId}&moderator_id=${client._data.userId}`,
+            headers: {
+                'Client-ID': `${client._data.applicationId}`,
+                'Authorization': `Bearer ${client._data.token}`
+            }
+        };
+        return new Promise((resolve, reject) => {
+            let response = "";
+            https.get(options, res => {
+                res.setEncoding('utf8');
+                res.on('data', data => { response += data; });
+                res.on('end', () => { try { const messages = JSON.parse(response).messages; resolve(messages); } catch (err) { reject(err); } });
+            }).on('error', err => { log.error(err); reject(err); });
+        });
     }
 
     async isChannelLive() {
