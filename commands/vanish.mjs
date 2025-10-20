4d7e5a4264c9a991f9fa623f330daabdcf6906c6
endymion2k14 <roland@desktop-amd>
2025-10-20 03:59:39 +0200

    put this in place, we got debug

--- commands/vanish.mjs
+++ commands/vanish.mjs
@@ -3,10 +3,7 @@ export default {
     name: 'vanish',
     systems: ['vanish'],
     async reply(params, client, event) {
-        // if (event.privileges.broadcaster || event.privileges.moderator) {
-        //     client.sendMessage(`Broadcasters and Moderators cannot vanish themselves ${event.username}.`);
-        //     return;
-        // }
+        if (event.privileges.broadcaster || event.privileges.moderator) { client.sendMessage(`Broadcasters and Moderators cannot vanish themselves ${event.username}.`); return; }
         const system = client.getSystem('vanish');
         if (system.data[client.channel] && system.data[client.channel].userMessages) {
             const userId = event.tags['user-id'];
@@ -20,7 +17,7 @@ export default {
                 //     }).catch(error => {
                 //         console.error(`Failed to remove message with ID ${message.id}:`, error);
                 //     });
-                });
+                // });
             } else {
                 console.log('No messages found for this user.');
             }
