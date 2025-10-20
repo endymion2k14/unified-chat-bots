56ae7d310880ea8b2e719937ee211f4e5b64da52
endymion2k14 <roland@desktop-amd>
2025-10-20 03:57:36 +0200

    update revamp system logic

--- systems/vanish.mjs
+++ systems/vanish.mjs
@@ -8,8 +8,8 @@ export default {
     data: {},
     init(client) {
         this.data[client.channel] = this.data[client.channel] || { live: false, userMessages: {} };
-        client.api.addListener(EventTypes.stream_start, status => { this.data[client.channel].live = true; this.data[client.channel].userMessages = {}; });
-        client.api.addListener(EventTypes.stream_end, status => { this.data[client.channel].live = false; this.data[client.channel].userMessages = {}; });
+        client.api.addListener(EventTypes.stream_start, status => { if (this.data[client.channel].live) { return; } this.data[client.channel].live = true; this.data[client.channel].userMessages = {}; });
+        client.api.addListener(EventTypes.stream_end, status => { if (!this.data[client.channel].live) { return; } this.data[client.channel].live = false; this.data[client.channel].userMessages = {}; });
         client.on('message', (event) => {
             const messageId = event.tags.id;
             this.data[client.channel].userMessages[event.tags['user-id']] = this.data[client.channel].userMessages[event.tags['user-id']] || [];
