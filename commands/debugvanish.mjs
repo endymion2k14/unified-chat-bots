0a6ca4d3ee14f0fd6bbbbfa007f133bb87b3412a
endymion2k14 <roland@desktop-amd>
2025-10-20 05:02:39 +0200

    rename

--- commands/debugvanish.mjs
+++ commands/debugvanish.mjs
@@ -1,5 +1,5 @@
 export default {
-    name: 'debug',
+    name: 'debugvanish',
     async reply(params, client, event) {
         if (event.privileges.super       ||
             event.privileges.broadcaster ||
