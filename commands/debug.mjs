366ce4dba79e8d5f2bd4abbebd7be1d68c9ed04c
endymion2k14 <roland@desktop-amd>
2025-10-20 04:34:08 +0200

    itnroduce gptrecap debugging

---
+++ commands/debug.mjs
@@ -0,0 +1,16 @@
+export default {
+    name: 'debug',
+    async reply(params, client, event) {
+        if (event.privileges.super       ||
+            event.privileges.broadcaster ||
+            event.privileges.moderator) {
+            // gptrecap Debugging
+            const gptrecap = client.getSystem('gptrecap');
+            if (gptrecap && gptrecap.data) {
+                 for (const key in gptrecap.data) {
+                    console.log(gptrecap.data[key]);
+                 }
+            } else { console.log('The gptrecap data is not available.'); }
+        } else { client.sendMessage(`You need to be at least a moderator to use this command ${event.username}.`); }
+    },
+};
