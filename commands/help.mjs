23832373266c36992b80af84bf9c5e962f27a5ae
endymion2k14 <roland@desktop-amd>
2025-11-16 16:36:07 +0100

    fix aliases being shown on help

--- commands/help.mjs
+++ commands/help.mjs
@@ -4,7 +4,7 @@ export default {
     name: 'help',
     async reply(params, client, event) {
         let commandList = [];
-        for (let i = 0; i < client._commands.length; i++) { if (event.privileges.super || !client._commands[i].hidden) { commandList.push(client._commands[i].name); } }
+        for (let i = 0; i < client._commands.length; i++) { if (event.privileges.super || !client._commands[i].hidden) { let mainName = client._commands[i].command.name; if (!commandList.includes(mainName)) { commandList.push(mainName); } } }
         client.sendMessage(`Possible commands: ${concat(commandList, ', ', client.prefix).toLowerCase()}`);
     }
 }
