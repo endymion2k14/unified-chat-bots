44ba5a8d1f0ea59192f0a70d2ad327b6553050ec
endymion2k14 <roland@desktop-amd>
2025-11-18 21:26:27 +0100

    401 on set title/game
    ??
    scopes are fine.

---
+++ commands/settitle.mjs
@@ -0,0 +1,20 @@
+import { concat, log } from '../utils.mjs';
+
+const SOURCE = 'settitle.mjs';
+
+export default {
+    name: 'settitle',
+    async reply(params, client, event) {
+        if (event.privileges.super || event.privileges.broadcaster || event.privileges.moderator) {
+            if (params.length > 0) {
+                try {
+                    const userInfo = await client.api.getAccountInfo(event.channel);
+                    client.api.setTitle(userInfo.id, concat(params, ' '));
+                } catch (err) {
+                    log.error(`Something went wrong trying to set the title: ${err}`, SOURCE);
+                    client.sendMessage(`Something went wrong trying to set the title: ${event.username}.`);
+                }
+            } else { client.sendMessage('You need to specify a new stream title.'); }
+        } else { client.sendMessage(`You need to be at least a moderator to use this command ${event.username}.`); }
+    }
+}
