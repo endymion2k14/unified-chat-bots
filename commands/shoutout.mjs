e0bc06ceea17b60277478d5ee3f79253ee492040
endymion2k14 <endymion2k14@localhost>
2025-12-07 04:18:31 +0100

    if we use bot token, then use announce instead

--- commands/shoutout.mjs
+++ commands/shoutout.mjs
@@ -15,10 +15,12 @@ export default {
                 let messageTemplate;
                 if (streamInfo) { lastPlayedGame = streamInfo.game_name; messageTemplate = config.streamingMessage || `BIG SHOUTOUT TO @${username}, they are currently streaming in the ${lastPlayedGame} category @ https://www.twitch.tv/${username}`; }
                 else { messageTemplate = config.offlineMessage || `BIG SHOUTOUT TO @${username}, ${lastPlayedGame} @ https://www.twitch.tv/${username}`; }
-                const message = messageTemplate.replace(/{username}/g, username).replace(/{lastplayed}/g, lastPlayedGame);
-                client.sendMessage(message);
-                // sendAnnouncement requires OAuth so is disabled in favor of 60 day token.
-                //client.api.sendAnnouncement(userInfo.id, message);
+                 const message = messageTemplate.replace(/{username}/g, username).replace(/{lastplayed}/g, lastPlayedGame);
+                 if (client._settings.settings.ircTokenSource === 'bot') {
+                     client.api.sendAnnouncement(userInfo.id, message);
+                 } else {
+                     client.sendMessage(message);
+                 }
             } catch (error) { client.sendMessage(`Error checking user: ${error}`); }
         } else { client.sendMessage(`You need to be at least a Moderator to use this command ${event.username}.`); }
     },
